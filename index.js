36ef0964c988bb3b99adfe1da0288d6c206a9fa4
the-wrong-guy <kaltbx@gmail.com>
2021-02-06 22:55:42 +0530

    first commit / added by Bhargab

---
+++ index.js
@@ -0,0 +1,9 @@
+export function imgTo64(img) {
+    const file = img;
+    let reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.readAsDataURL(file);
+      reader.onloadend = () => resolve(reader.result);
+    });
+  }
+  
